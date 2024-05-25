a2973becc4820a424599924a35650a797b18cfdc
cillian <cillian@otainsight.com>
2024-05-25 16:00:53 +0200

    material added

--- iLead/src/app/app-routing.module.ts
+++ iLead/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from '@pages/page-not-found/page-not-found.component';
-import { ROUTES } from '@app/routes';
+import { HomeComponent } from './pages/home/home.component';
+import { FormDefinitionComponent } from './pages/form-definition/form-definition.component';
 
 const routes: Routes = [
-  { path: 'home', component: ROUTES.HomeComponent },
-  { path: 'form-definition', component: ROUTES.FormDefinitionComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'form-definition', component: FormDefinitionComponent },
   { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
 
--- iLead/src/app/app.module.ts
+++ iLead/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { FormDefinitionComponent } from './pages/form-definition/form-definition.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormDefinitionComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSlideToggleModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
