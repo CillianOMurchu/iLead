40d002167d18175ab40875474bb57684272a6d0e
cillian <cillian@otainsight.com>
2024-05-25 15:18:22 +0200

    routing working

--- iLead/src/app/app-routing.module.ts
+++ iLead/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ROUTES } from './routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ROUTES.HomeComponent },
+  { path: 'form-definition', component: ROUTES.FormDefinitionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ iLead/src/app/routes.ts
@@ -0,0 +1,4 @@
+import { HomeComponent } from './admin/pages/home/home.component';
+import { FormDefinitionComponent } from './admin/pages/form-definition/form-definition.component';
+
+export const ROUTES = { HomeComponent, FormDefinitionComponent };
