5f5a9ba0aa8448ca32b42b4753e5fbbba3e1d74e
cillian <cillian@otainsight.com>
2024-10-18 17:10:24 +0200

    remove api key

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -8,7 +8,7 @@ import OpenAI from 'openai';
 })
 export class MessageService {
   private apiUrl =
-    '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';
+    'API_KEY_HERE';
 
   mappedPrompt = '';
 
